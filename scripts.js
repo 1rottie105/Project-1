374e83fb1f440fd5cc3e129a07a3807a9e704315
Kaelib <kaelibw84@gmail.com>
2019-10-26 19:46:56 -0500

    Attempt to resolve persisting JS error

--- scripts.js
+++ scripts.js
@@ -1,39 +1,32 @@
 // Calculator program to determine Tip value by specific percentages.
 
 function FindTip(num1, num2) {
-  let tip = num1 * num2;
-  return tip;
+  return num1 * num2;
 }
-function ConvertPercent(num1)
-{
-  let percent = 0;
-  if(num1 = 'fifteen')
-  {
-    percent = 0.15;
-  }
-  else if(num1 = 'twenty')
-  {
-    percent = 0.2;
-  }
-  else if(num1 = 'twenty-five')
-  {
-    percent = 0.25;
+function ConvertPercent(num1) {
+  if (num1 == "fifteen") {
+    return 0.15;
+  } else if (num1 == "twenty") {
+    return 0.2;
+  } else if (num1 == "twenty-five") {
+    return 0.25;
   }
 }
-function FindTotal(num1, num2)
-{
-  let total = num1 + num2;
-  return total;
+function FindTotal(num1, num2) {
+  return num1 + num2;
 }
 const form = document.querySelector("form");
 const bill = document.querySelector("#bill");
-const percent = document.querySelector("#percentage");
+const percentage = document.querySelector("#percentage");
 const tip = document.querySelector("#tip");
 const total = document.querySelector("#total");
 
-form.addEventListener("submit", function event){
-event.preventDefault();
-console.log(bill.value, percent.value, tip.value, total.value)
-tip.textContent = FindTip(bill.value,ConvertPercent(percent.value))
-total.textContent = FindTotal(bill.value, FindTip(bill.value, ConvertPercent(percent.value)))
-}
+form.addEventListener("submit", function event() {
+  event.preventDefault();
+  console.log(bill.value, percentage.value, tip.value, total.value);
+  tip.textContent = FindTip(bill.value, ConvertPercent(percentage.value));
+  total.textContent = FindTotal(
+    bill.value,
+    FindTip(bill.value, ConvertPercent(percentage.value))
+  );
+});
